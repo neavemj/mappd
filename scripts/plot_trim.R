490df2296547e6acdf25f8e91dd186ca267bcbd0
neavemj <matthewjneave1@gmail.com>
2019-03-21 15:45:44 +1100

    added assembly rule

--- scripts/plot_trim.R
+++ scripts/plot_trim.R
@@ -10,17 +10,19 @@ plot_trim <- function(trim_summary, pdf_file, png_file) {
   
   summary_df = read.table(trim_summary, sep="\t", header=T)
 
-  summary_long = gather(summary_df, type, pairs, both_surviving, forward_only, reverse_only, dropped)
+  summary_long = gather(summary_df, Result, pairs, both_surviving, forward_only, reverse_only, dropped)
 
-  summary_long$type <- factor(summary_long$type, levels = c("dropped", "reverse_only", "forward_only", "both_surviving"))
+  summary_long$Result <- factor(summary_long$Result, levels = c("dropped", "reverse_only", "forward_only", "both_surviving"))
 
   cols <- c("both_surviving" = "#7CAE00", "forward_only" = "#C77CFF", "reverse_only" = "#00BFC4", "dropped" = "#F8766D")
   
-  p <- ggplot(summary_long, aes(x=sample, y=pairs, fill=type)) +
+  p <- ggplot(summary_long, aes(x=sample, y=pairs, fill=Result)) +
     geom_bar(stat='identity') +
     scale_y_continuous(labels = comma) +
     scale_fill_manual(values=cols) +
-    theme(axis.text.x = element_text(angle=45, hjust=1))
+    theme(axis.text.x = element_text(angle=45, hjust=1)) +
+    ylab("Read pairs") +
+    xlab("Sample")
   
   # make the figure width adjust dynamically with the number of samples
   # add 1/3 of an inch for each additional samples
